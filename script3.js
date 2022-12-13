daa9740f0d3dad4f923355b79fc433d9fc75df25
mavlianov20001settings <jagu68835@gmail.com>
2022-12-13 15:14:41 +0600

    hw3

---
+++ script3.js
@@ -0,0 +1,68 @@
+// let s = Number(prompt("s"));
+// let f = Number(prompt("f"));
+// function calc(s, f) {
+//   return alert(s + f);
+//   return alert(s - f);
+//   return alert(s * f);
+//   return alert(s / f);
+// }
+// let a = Number(prompt("введитe 1ое число ")); // firstNumber
+// let calcu = prompt("введите математический символ: +, -, *, / "); // знак операции
+// let b = Number(prompt("введите 2ое число")); // secondNumber
+// let с = "eror";
+// function calc(a, b, calcu, с) {
+//   switch (calcu) {
+//     case "+":
+//       с = a + b;
+//       break;
+//     case "-":
+//       с = a - b;
+//       break;
+//     case "*":
+//       с = a * b;
+//     case "/":
+//       if (b === 0) {
+//         console.log("нельзя делить на 0!");
+//         break;
+//       }
+//       с = a / b;
+//       break;
+//     default:
+//       console.log("eror");
+//       break;
+//   }
+//   return alert(с);
+// }
+// if (isNaN(a) || isNaN(b)) {
+//   alert("вы должны ввести числа!");
+// } else calc(a, b, calcu, с);
+//
+//
+//
+// let q = Number(prompt("введите 1ое число"));
+// let w = Number(prompt("введите 2ое число"));
+// function s(q, w) {
+//   if (isNaN(q) || isNaN(w)) {
+//     console.log("число!!!");
+//   } else if (q > w) {
+//     console.log(w);
+//   } else if (q < w) {
+//     console.log(q);
+//   } else if ((q = w)) {
+//     console.log("они равны");
+//   }
+// }
+// s(q, w);
+//
+//
+//
+//
+//
+//
+//
+//
+// let test = prompt("");
+// function a(test) {
+//   return console.log(test.length);
+// }
+// a(test);
